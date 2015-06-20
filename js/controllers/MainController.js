a8dee6fafffb440563fe906571ecc1aae67e61b9
Joshua Snider <jsnider3@masonlive.gmu.edu>
2015-06-20 02:22:25 -0400

    Workingish, map needs to be inverted.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,7 @@
 app.controller('MainController', 
                ['$scope', '$http', function($scope, $http) {
                   $scope.github = "https://api.github.com",
-                  $scope.main = function () {
+                  $scope.main = function (username) {
                     var gitUrl = "./repos.json"
                     //var gitUrl = $scope.github + "/users/" + username + "/repos"
                     $http.get(gitUrl).then(function(resp) {
@@ -13,51 +13,105 @@ app.controller('MainController',
                     var links = repos.map(function (repo) {
                       return repo.full_name;
                     });
-                    document.getElementById('results').value = links.join();
+                    repos = links.map(function (link) {
+                      return link.split("/")[1];
+                    });
                     var count = links.length;
                     var progmaps = {};
                     links.forEach(function (link) {
                       //For each repo.
                       //var langUrl = $scope.github + "/repos/" + link + "/languages"
+                      var reponame = link.split("/")[1] 
                       var langUrl = "./" + link + ".json"
-                      //console.log(langUrl)
                       $http.get(langUrl).then(function(resp) {
                         var langs = resp.data
                         console.log(langs)
                         count--;
                         if (Object.keys(langs).length > 0) {
                           //Keep track of how much code was written in each language.
-                          //TODO Rewrite this garbage.
-                          Object.keys(langs).forEach(function (k) {
-                            if (k in progmaps) {
-                              progmaps[k][link] = langs[k]//+= langs[k];
+                          Object.keys(langs).forEach(function (lang) {
+                            if (lang in progmaps) {
                             } else {
-                              progmaps[k] = {}
-                              progmaps[k][link] = langs[k];
+                              progmaps[lang] = []
                             }
+                            progmaps[lang].push({name: reponame, value: langs[lang]})
                           });
-                          
-                          if (count == 0) {
-                            var order = Objects.key(progmaps).sort(function (a, b) {
-                              var sum = function(k) {
-                                
+                        }
+                        if (count == 0) {
+                          console.log(progmaps)
+                          var order = Object.keys(progmaps).sort(function (a, b) {
+                            var sum = function(lang) {
+                              var cnt = 0
+                              progmaps[lang].forEach( function(repo) {
+                                cnt += repo.value
+                              })
+                              return cnt 
+                            }
+                            return sum(b) - sum(a);
+                          }); 
+                          /*var chartData = {}
+                          Object.keys(progmaps).forEach( function(lang) {
+                            Object.keys(progmaps[lang)
+                          })*/
+                          Object.keys(progmaps).forEach(function(lang) {
+                            repos.forEach(function(repo) {
+                              var found = false
+                              progmaps[lang].forEach(function(elem) {
+                                if (elem.name === repo) {
+                                  found = true
+                                }
+                              })
+                              if (!found) {
+                                progmaps[lang].push({name: repo, value: 0})
                               }
-                              return sum(b) - sum(a);
-                            });
-                            //Done, sort them, and return them.
-                            /*var langs = Object.keys(progmaps).map(function (k) {
-                              return {lang: k, bytes: progmaps[k]};
-                            });
-                            langs.sort(function(a, b) {
-                              return b.bytes - a.bytes;
-                            });*/
-                            console.log("Order")
-                            console.log(order)
-                            console.log("Data")
-                            console.log(progmaps);//langs);
-                          }
+                            })
+                            progmaps[lang].sort()
+                          })
+                          $scope.makeChart(order, progmaps);
                         }
                       });
                     });
-                  };
+                  },
+                  
+                  $scope.makeChart = function(order, data) {
+                    console.log("Chartify!")
+                    console.log("Order")
+                    console.log(order)
+                    console.log("Data")
+                    console.log(JSON.stringify(data));
+                    var graphdef = {
+                      categories : order,//['C++', 'TeX'],
+                      dataset : data/*{
+                        'C++' : [
+                          { name : 'workspace', value : 32 },
+                          { name : 'study', value : 60 },
+                          { name : '2011', value : 97 },
+                          { name : '2012', value : 560 },
+                          { name : '2013', value : 999 }
+                        ],
+                        'TeX' : [
+                          { name : 'workspace', value : 999 },
+                          { name : 'study', value : 97 },
+                          { name : '2011', value : 560 },
+                          { name : '2012', value : 60 },
+                          { name : '2013', value : 32 },
+                          { name : '2014', value : 32 }
+                        ]
+                      }*/
+                    }
+                    var chart = uv.chart('StackedBar', graphdef, {
+                      meta: {
+                        caption: 'Your Most Used Languages',
+                        subcaption: 'sorted by repo',
+                        hlabel: 'Size',
+                        hsublabel: 'Bytes',
+                        vlabel: 'Languages'
+                      },
+                      dimension: {
+                        width: 600,
+                        height: 600
+                      }
+                    });
+                  }
+
                 }])
--- notes.md
+++ notes.md
@@ -11,3 +11,7 @@ synchronous.
 Ever look at some JavaScript file and see that it looks like line noise?
 That's called "minification" and it's basically like compression except
 it doesn't need to be undone for you to interpret it.
+
+uvCharts needs to have zeros for "StackedBar" values that aren't present.
+Also, the values need to be in the same order for everyone. It turns out
+I still need to invert my things.
