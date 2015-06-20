e4106db3cdce0271df794922e273602023d39088
Joshua Snider <jsnider3@masonlive.gmu.edu>
2015-06-20 13:59:12 -0400

    Works, now to make it pretty

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,15 +1,26 @@
 app.controller('MainController', 
                ['$scope', '$http', function($scope, $http) {
                   $scope.github = "https://api.github.com",
-                  $scope.main = function (username) {
-                    var gitUrl = "./repos.json"
-                    //var gitUrl = $scope.github + "/users/" + username + "/repos"
+                  $scope.main = function () {
+                    var username = document.getElementById("user").value
+                    var gitUrl = $scope.github + "/users/" + username + "/repos"
+                    gitUrl = "./repos.json"
+                    console.log(gitUrl)
                     $http.get(gitUrl).then(function(resp) {
                       $scope.repoCallback(resp.data)
                     })
                   },
-
+                  $scope.findNamed = function(arr, name) {
+                    var result = undefined
+                    arr.forEach(function(elem) {
+                      if (elem.name == name) {
+                        result = elem
+                      }
+                    })
+                    return result
+                  },
                   $scope.repoCallback = function(repos) {
+                    console.log(repos)
                     var links = repos.map(function (repo) {
                       return repo.full_name;
                     });
@@ -18,86 +29,70 @@ app.controller('MainController',
                     });
                     var count = links.length;
                     var progmaps = {};
+                    var langSet = []
                     links.forEach(function (link) {
                       //For each repo.
-                      //var langUrl = $scope.github + "/repos/" + link + "/languages"
+                      var langUrl = $scope.github + "/repos/" + link + "/languages"
                       var reponame = link.split("/")[1] 
-                      var langUrl = "./" + link + ".json"
-                      $http.get(langUrl).then(function(resp) {
-                        var langs = resp.data
-                        console.log(langs)
+                      langUrl = "./" + link + ".json"
+                      $http.get(langUrl).then(function(langs) {
+                        var langs = langs.data
                         count--;
                         if (Object.keys(langs).length > 0) {
                           //Keep track of how much code was written in each language.
                           Object.keys(langs).forEach(function (lang) {
-                            if (lang in progmaps) {
-                            } else {
-                              progmaps[lang] = []
+                            if (!progmaps.hasOwnProperty(reponame)) {
+                              progmaps[reponame] = []
                             }
-                            progmaps[lang].push({name: reponame, value: langs[lang]})
+                            progmaps[reponame].push({name: lang, value: langs[lang]})
+                            langSet.push(lang)
                           });
                         }
                         if (count == 0) {
-                          console.log(progmaps)
-                          var order = Object.keys(progmaps).sort(function (a, b) {
-                            var sum = function(lang) {
-                              var cnt = 0
-                              progmaps[lang].forEach( function(repo) {
-                                cnt += repo.value
-                              })
-                              return cnt 
-                            }
-                            return sum(b) - sum(a);
-                          }); 
-                          /*var chartData = {}
-                          Object.keys(progmaps).forEach( function(lang) {
-                            Object.keys(progmaps[lang)
-                          })*/
-                          Object.keys(progmaps).forEach(function(lang) {
-                            repos.forEach(function(repo) {
-                              var found = false
-                              progmaps[lang].forEach(function(elem) {
-                                if (elem.name === repo) {
-                                  found = true
-                                }
-                              })
-                              if (!found) {
-                                progmaps[lang].push({name: repo, value: 0})
+                          function onlyUnique(value, index, self) {
+                            return self.indexOf(value) === index;
+                          }
+                          repos = repos.filter(onlyUnique)
+                          langSet = langSet.filter(onlyUnique)
+
+                          $scope.sortLangs(progmaps, langSet)
+                          Object.keys(progmaps).forEach(function(reponame) {
+                            langSet.forEach(function(lingua) {
+                              var found = $scope.findNamed(progmaps[reponame], lingua)
+                              if (found == undefined) {
+                                progmaps[reponame].push({name: lingua, value: 0})
                               }
                             })
-                            progmaps[lang].sort()
+                            progmaps[reponame].sort(function (a, b) {
+                              return langSet.indexOf(b.name) - langSet.indexOf(a.name)
+                            })
                           })
-                          $scope.makeChart(order, progmaps);
+                          $scope.makeChart(repos, progmaps);
                         }
                       });
                     });
                   },
-                  
+
+                  $scope.sortLangs = function(repos, langs) {
+                    langs = langs.sort(function (a, b) {
+                      var sum = function(lang) {
+                        var cnt = 0
+                        Object.keys(repos).forEach( function(reponame) {
+                          var search = $scope.findNamed(repos[reponame], lang)
+                          if (search != undefined) {
+                            cnt += search.value
+                          }
+                        })
+                        return cnt
+                      }
+                      return sum(a) - sum(b);
+                    });
+                  },
+
                   $scope.makeChart = function(order, data) {
-                    console.log("Chartify!")
-                    console.log("Order")
-                    console.log(order)
-                    console.log("Data")
-                    console.log(JSON.stringify(data));
                     var graphdef = {
-                      categories : order,//['C++', 'TeX'],
-                      dataset : data/*{
-                        'C++' : [
-                          { name : 'workspace', value : 32 },
-                          { name : 'study', value : 60 },
-                          { name : '2011', value : 97 },
-                          { name : '2012', value : 560 },
-                          { name : '2013', value : 999 }
-                        ],
-                        'TeX' : [
-                          { name : 'workspace', value : 999 },
-                          { name : 'study', value : 97 },
-                          { name : '2011', value : 560 },
-                          { name : '2012', value : 60 },
-                          { name : '2013', value : 32 },
-                          { name : '2014', value : 32 }
-                        ]
-                      }*/
+                      categories : order,
+                      dataset : data
                     }
                     var chart = uv.chart('StackedBar', graphdef, {
                       meta: {
@@ -113,5 +108,4 @@ app.controller('MainController',
                       }
                     });
                   }
-
                 }])
